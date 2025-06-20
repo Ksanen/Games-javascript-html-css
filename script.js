799f7aae95814bfa4255b159f4456a4ab773be79
KsawierCz <ksawiercz1222@gmail.com>
2025-06-20 16:51:19 +0200

    adding script that changes pages and fixing small css issue

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ class VisualMemoryGame {
 class UIManager {
     constructor(app) {
         this.app = app;
+        this.pages = {
+            game: document.querySelector(`[page="game"`),
+            gameOver: document.querySelector(`[page="game-over"`),
+            settings: document.querySelector(`[page="settings"`),
+            start: document.querySelector(`[page="start"`)
+        }
+    }
+    changePage(newPage) {
+        for (const page in this.pages) {
+            if (this.pages[page].getAttribute("page") === newPage) {
+                this.showPage(this.pages[page]);
+            }
+            else {
+                this.hidePage(this.pages[page]);
+            }
+        }
+    }
+    hidePage(page) {
+        page.classList.add("page--invisible");
+    }
+    showPage(page) {
+        page.classList.remove("page--invisible");
     }
 }
 class EventManager {
@@ -14,3 +36,4 @@ class EventManager {
         this.app = app;
     }
 }
+const visual = new VisualMemoryGame();
