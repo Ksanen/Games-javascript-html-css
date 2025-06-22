9364bdea6c01acbe9531b8798fc07437ddff052f
KsawierCz <ksawiercz1222@gmail.com>
2025-06-22 21:45:54 +0200

    adding hearts svg instead of number and adding you lost information

--- VisualMemory/script.js
+++ VisualMemory/script.js
@@ -30,9 +30,10 @@ class VisualMemoryGame {
         this.selectRandomFields();
         this.ui.showingFieldAnimation();
         this.ui.lockBoard();
+        this.ui.generateHearts();
     }
     startGame() {
-        this.ui.upgradeHeartsCounter();
+        this.ui.upgradeHearts();
         this.ui.upgradeRoundCounter();
         this.prepareBoard();
         this.ui.changePage("game");
@@ -70,10 +71,10 @@ class VisualMemoryGame {
     }
     resetBoard = () => {
         this.ui.clearTimeouts();
-        this.ui.upgradeHeartsCounter();
+        this.ui.upgradeHearts();
         this.ui.upgradeRoundCounter();
         this.unlockAllFields();
-        document.querySelector(`[page="game"]`).classList.remove("game--lose")
+        document.querySelector(`.info`).classList.remove("info--show")
     }
     unlockAllFields() {
         this.fieldsThatAreBlocked.clear();
@@ -87,10 +88,10 @@ class VisualMemoryGame {
     }
     decreaseHearts() {
         this.settings.hearts--;
-        this.ui.upgradeHeartsCounter();
+        this.ui.upgradeHearts();
         if (this.settings.hearts === 0) {
             this.ui.lockBoard();
-            document.querySelector(`[page="game"]`).classList.add("game--lose")
+            document.querySelector(`.info`).classList.add("info--show")
         }
     }
     resetSettings() {
@@ -136,9 +137,6 @@ class UIManager {
     upgradeRoundCounter() {
         this.roundCounter.innerHTML = `Round: ${this.app.round}`;
     }
-    upgradeHeartsCounter() {
-        this.hearts.innerHTML = `Hearts: ${this.app.settings.hearts}`;
-    }
     selectField(field) {
         field.classList.add("field--selected");
     }
@@ -159,6 +157,26 @@ class UIManager {
     }
     unlockBoard = () => {
         this.board.classList.remove("game_board--locked");
+    }
+    upgradeHearts() {
+        const hearts = document.querySelectorAll(".heart");
+        hearts.forEach((heart, index) => {
+            if (index >= this.app.settings.hearts) {
+                heart.classList.add("heart--none");
+            }
+            else {
+                heart.classList.remove("heart--none");
+            }
+        })
+    }
+    generateHearts() {
+        this.hearts.innerHTML = "";
+        for (let i = 0; i < this.app.settings.hearts; i++) {
+            this.hearts.innerHTML += `<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="heart">
+                <path
+                    d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z" />
+            </svg>`;
+        }
     }
     generateBoard(columns, rows) {
         this.board.innerHTML = "";
